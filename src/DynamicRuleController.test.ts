68bcb884c6e71284925feae746d56fce88a2e033
Pahaz <pahaz.white@gmail.com>
2022-01-22 12:04:57 +0500

    🥶 DynamicRuleController add rules validation

--- src/DynamicRuleController.test.ts
+++ src/DynamicRuleController.test.ts
@@ -4,7 +4,7 @@ import { TestController } from './main.test'
 import { StorageController } from './StorageController'
 import { TelegramController } from './TelegramController'
 
-test('DynamicRuleController.init()', async () => {
+async function makeInitedDynamicRuleController () {
     const app = express()
     const controller = new RuleController({
         serverUrl: 'https://localhost:3001',
@@ -17,5 +17,62 @@ test('DynamicRuleController.init()', async () => {
         howToUpdateRule: [],
     })
     await controller.init(app)
+    return controller
+}
+
+test('DynamicRuleController.init()', async () => {
+    const controller = await makeInitedDynamicRuleController()
     expect(controller.name).toEqual('_rule')
+})
+
+test('DynamicRuleController validation action', async () => {
+    const controller = await makeInitedDynamicRuleController()
+    const result = await controller.action('_updateRules', {
+        namespace: 'test',
+        rules: JSON.stringify([
+            {
+                controller: 'telegram',
+                when: 'message',
+                case: '{{ chat.id and document.mime_type === "multipart/x-zip" and document.file_size < 12300100 and document.file_name.endsWith("serverless.zip") }}',
+                do: [
+                    {
+                        controller: 'telegram',
+                        action: '_temporarilyDownloadFileLocally',
+                        as: 'localFilePath',
+                        args: {
+                            fileId: '{{ document.file_id }}',
+                        },
+                    },
+
+                ],
+            },
+        ]),
+    })
+    expect(result).toEqual('AssertionError [ERR_ASSERTION]: rules: "do"."action" name should not starts with _')
+})
+
+test('DynamicRuleController validation controller', async () => {
+    const controller = await makeInitedDynamicRuleController()
+    const result = await controller.action('_updateRules', {
+        namespace: 'test',
+        rules: JSON.stringify([
+            {
+                controller: '_rules',
+                when: 'message',
+                case: '{{ chat.id and document.mime_type === "multipart/x-zip" and document.file_size < 12300100 and document.file_name.endsWith("serverless.zip") }}',
+                do: [
+                    {
+                        'controller': 'telegram',
+                        'action': 'sendMessage',
+                        'args': {
+                            'chatId': '{{ chat.id }}',
+                            'text': 'Hi from `{{ chat.id }}`',
+                            'mode': 'Markdown',
+                        },
+                    },
+                ],
+            },
+        ]),
+    })
+    expect(result).toEqual('AssertionError [ERR_ASSERTION]: rules: "controller" name should not starts with _')
 })
--- src/DynamicRuleController.ts
+++ src/DynamicRuleController.ts
@@ -1,6 +1,7 @@
 import assert from 'assert'
+import { Mutex } from 'async-mutex'
 import { Express } from 'express'
-import { fromPairs, isArray } from 'lodash'
+import { fromPairs, isArray, isObject, toPairs } from 'lodash'
 
 import { BaseEventController, BaseEventControllerOptions } from './BaseEventController'
 import { getLogger } from './logger'
@@ -8,12 +9,28 @@ import { Rules, setupRules, updateRules } from './main'
 
 const STORAGE_RULE_PATH_PREFIX = 'rules'
 const logger = getLogger('rule')
+const writeMutex = new Mutex()
 
 
 function validateNamespaceAndRules (namespace: string, ruleObjects: Rules) {
     const namespaceValidator = /^[\w-_]+$/g
     assert.ok(namespaceValidator.test(namespace), 'namespace is not valid')
     assert.ok(isArray(ruleObjects), 'rules json is not an array')
+    for (const rule of ruleObjects) {
+        assert.ok(!rule.controller.startsWith('_'), 'rules: "controller" name should not starts with _')
+        assert.ok(!rule.when.startsWith('_'), 'rules: "when" condition should not starts with _')
+        assert.ok(!rule.case || !rule.case.startsWith('_'), 'rules: "case" condition should not starts with _')
+        assert.ok(Array.isArray(rule.do), 'rules: "do" is not an array')
+        for (const does of rule.do) {
+            assert.ok(!does.controller.startsWith('_'), 'rules: "do"."controller" name should not starts with _')
+            assert.ok(!does.action.startsWith('_'), 'rules: "do"."action" name should not starts with _')
+            assert.ok(!isObject(does.args), 'rules: "do"."args" is not an object')
+            toPairs(does.args).forEach(([key, val]) => {
+                assert.ok(typeof val === 'string', `rules: "do"."args" (${key}) value is not a string`)
+                assert.ok(!key.startsWith('_'), `rules: "do"."args" (${key}) should not starts with _`)
+            })
+        }
+    }
 }
 
 interface RuleControllerOptions extends BaseEventControllerOptions {
@@ -57,12 +74,21 @@ class RuleController extends BaseEventController {
     async action (name: string, args: { namespace: string, rules: string, _message?: string }): Promise<any> {
         logger.debug({ controller: this.name, step: 'action()', action: name, args })
         if (name === '_updateRules') {
-            const ruleObjects = JSON.parse(args.rules)
-            validateNamespaceAndRules(args.namespace, ruleObjects)
-            const disposers = this.namespaces[args.namespace] || []
-            this.namespaces[args.namespace] = await updateRules(ruleObjects, disposers, this.controllers)
-            await this.storage.action('writeJson', { path: `${STORAGE_RULE_PATH_PREFIX}/${args.namespace}`, value: ruleObjects, _message: args._message })
-            return this.namespaces[args.namespace].map(x => x.ruleId)
+            const release = await writeMutex.acquire()
+            try {
+                const ruleObjects = JSON.parse(args.rules)
+                validateNamespaceAndRules(args.namespace, ruleObjects)
+                const disposers = this.namespaces[args.namespace] || []
+                this.namespaces[args.namespace] = await updateRules(ruleObjects, disposers, this.controllers)
+                await this.storage.action('writeJson', { path: `${STORAGE_RULE_PATH_PREFIX}/${args.namespace}`, value: ruleObjects, _message: args._message })
+                const ruleIds: Array<string> = this.namespaces[args.namespace].map(x => x.ruleId)
+                ruleIds.sort()
+                return ruleIds.join('\n')
+            } catch (error) {
+                return error.toString()
+            } finally {
+                release()
+            }
         } else {
             throw new Error(`unknown action name: ${name}`)
         }
