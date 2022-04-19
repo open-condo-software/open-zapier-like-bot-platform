9966300ee927c6652437edc1560232cd742325f6
Pahaz <pahaz.white@gmail.com>
2022-04-19 23:35:56 +0300

    Add event storage list API

--- src/EventStorageController.ts
+++ src/EventStorageController.ts
@@ -17,6 +17,8 @@ interface EventStorageControllerOptions extends BaseEventControllerOptions {
     storageController: BaseEventController
     telegramController: BaseEventController
     skip?: (event: any) => boolean,
+    webEventAccessToken?: string,
+    webEventListAccessToken?: string,
 }
 
 class EventStorageController extends BaseEventController {
@@ -31,6 +33,8 @@ class EventStorageController extends BaseEventController {
     private onEventSendIndex: number
     private onEventSendTimeoutHandler: NodeJS.Timeout
     private skip: (event: any) => boolean
+    private webEventAccessToken: string
+    private webEventListAccessToken: string
 
     constructor (private options: EventStorageControllerOptions) {
         super(options)
@@ -45,6 +49,8 @@ class EventStorageController extends BaseEventController {
         this.onEventSendIndex = 0
         this.onEventSendDelay = options.onEventSendDelay || 3000
         this.onEventSendToTelegramChatId = options.onEventSendToTelegramChatId
+        this.webEventAccessToken = options.webEventAccessToken
+        this.webEventListAccessToken = options.webEventListAccessToken
         this.skip = options.skip
     }
 
@@ -59,15 +65,30 @@ class EventStorageController extends BaseEventController {
                     logger.debug({ controller: this.name, step: 'SKIPPED', eventId, eventController: controller, eventWhen: when })
                     return
                 }
-                this.memoryEvents[this.memoryEventIndex++ % MAX_MEMORY_EVENTS] = event
+                this.memoryEventIndex = (this.memoryEventIndex + 1) % MAX_MEMORY_EVENTS
+                this.memoryEvents[this.memoryEventIndex] = event
                 if (!this.onEventSendTimeoutHandler) {
                     const onEventSendMessage = async () => {
-                        if (this.onEventSendIndex >= this.memoryEventIndex) throw new Error('index problem!')
-                        const newEvents = this.memoryEvents.slice(this.onEventSendIndex, this.memoryEventIndex)
+                        let newEvents
+                        if (this.onEventSendIndex < this.memoryEventIndex) {
+                            newEvents = this.memoryEvents.slice(this.onEventSendIndex, this.memoryEventIndex)
+                        } else if (this.onEventSendIndex === this.memoryEventIndex) {
+                            logger.error({
+                                controller: this.name,
+                                step: 'onSendEventMessage() index === sentIndex',
+                                index: this.memoryEventIndex,
+                                sentIndex: this.onEventSendIndex,
+                            })
+                        } else if (this.onEventSendIndex > this.memoryEventIndex) {
+                            // NOTE: index overflow!
+                            newEvents = this.memoryEvents.slice(this.onEventSendIndex, MAX_MEMORY_EVENTS)
+                            newEvents = newEvents.concat(this.memoryEvents.slice(0, this.memoryEventIndex))
+                        }
+
                         this.onEventSendIndex = this.memoryEventIndex
                         this.onEventSendTimeoutHandler = null
                         const text = newEvents
-                            .map(({ id: eventId, controller, when }) => `<code>${controller}</code>:<code>${when}</code>:<a href="${this.serverUrl}/_event/${controller}/${when}/${eventId}">${eventId}</a>`)
+                            .map(({ id: eventId, controller, when }) => `<code>${controller}</code>:<code>${when}</code>:<a href="${this.serverUrl}/_event/${controller}/${when}/${eventId}?token=${this.webEventAccessToken}">${eventId}</a>`)
                             .join('\n')
 
                         try {
@@ -81,6 +102,8 @@ class EventStorageController extends BaseEventController {
                                 controller: this.name,
                                 step: 'onSendEventMessage()',
                                 error: serializeError(error),
+                                index: this.memoryEventIndex,
+                                sentIndex: this.onEventSendIndex,
                             })
                         }
                     }
@@ -101,7 +124,25 @@ class EventStorageController extends BaseEventController {
         }
         app.get('/_event/:controller/:when/:id', async (request, response) => {
             try {
+                if (!this.webEventAccessToken) {
+                    // NOTE: we don't give access to the event list without token config!
+                    response
+                        .writeHead(500, { 'content-type': 'application/json' })
+                        .end(JSON.stringify({ message: 'wrong server token config' }))
+                    return
+                }
+
                 const { id: eventId, controller, when } = request.params
+                if (this.webEventAccessToken) {
+                    const token = request.query.token
+                    if (!token || token !== this.webEventAccessToken) {
+                        response
+                            .writeHead(403, { 'content-type': 'application/json' })
+                            .end(JSON.stringify({ message: 'wrong access token', token }))
+                        return
+                    }
+                }
+
                 for (const event of this.memoryEvents) {
                     if (!event) break
                     if (event.id === eventId) {
@@ -119,6 +160,40 @@ class EventStorageController extends BaseEventController {
                     return
                 }
                 response.status(404).end()
+            } catch (error) {
+                logger.error({ controller: this.name, step: 'onAPIResult()', error: serializeError(error) })
+                response.status(500).end()
+            }
+        })
+        app.get('/_last_events', async (request, response) => {
+            try {
+                if (!this.webEventListAccessToken) {
+                    // NOTE: we don't give access to the last events list without token config!
+                    response
+                        .writeHead(500, { 'content-type': 'application/json' })
+                        .end(JSON.stringify({ message: 'wrong server token config' }))
+                    return
+                }
+                if (this.webEventListAccessToken) {
+                    const token = request.query.token
+                    if (!token || token !== this.webEventListAccessToken) {
+                        response
+                            .writeHead(403, { 'content-type': 'application/json' })
+                            .end(JSON.stringify({ message: 'wrong access token' }))
+                        return
+                    }
+                }
+
+                let events = this.memoryEvents.slice(0, this.memoryEventIndex)
+                if (this.memoryEvents[this.memoryEventIndex + 1] && this.memoryEventIndex + 1 < MAX_MEMORY_EVENTS) {
+                    events = this.memoryEvents.slice(this.memoryEventIndex + 1, MAX_MEMORY_EVENTS).concat(events)
+                }
+
+                const urls = events.map(({ id: eventId, controller, when }) => `${this.serverUrl}/_event/${controller}/${when}/${eventId}?token=${this.webEventAccessToken}`)
+                response
+                    .writeHead(200, { 'content-type': 'application/json' })
+                    .end(JSON.stringify(urls))
+                return
             } catch (error) {
                 logger.error({ controller: this.name, step: 'onAPIResult()', error: serializeError(error) })
                 response.status(500).end()
