557ed03268d416a712522d6943bf0f3bb92f83c0
Pahaz <pahaz.white@gmail.com>
2022-07-31 17:50:52 +0300

    logger: disable default log to file transport

--- src/logger.ts
+++ src/logger.ts
@@ -1,25 +1,6 @@
 import pino from 'pino'
 
-const transport = pino.transport({
-    targets: [
-        {
-            level: 'debug',
-            target: 'pino/file',
-            options: {
-                destination: './main.log.txt',
-            },
-        }, {
-            level: 'info',
-            target: 'pino-pretty',
-            options: {},
-        },
-    ],
-})
-
-const baseLogger = pino(transport)
-baseLogger.level = 'debug'
-
-const logger = baseLogger.child({ name: 'bot' })
+const logger = pino({ name: 'bot' })
 
 function getLogger (name: string): pino.Logger {
     return logger.child({ name: 'bot/' + name })
