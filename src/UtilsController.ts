32b0b57b6af791e2f82c8b7ff52db4b2be8cccd8
Pahaz <pahaz.white@gmail.com>
2022-07-31 17:51:52 +0300

    UtilsController: use new action style

--- src/UtilsController.ts
+++ src/UtilsController.ts
@@ -8,6 +8,23 @@ const logger = getLogger('utils')
 
 type UtilsControllerOptions = BaseEventControllerOptions
 
+interface MatchActionArgs {
+    text: string
+    pattern: string
+}
+
+interface MatchResult {
+    [key: string]: any
+}
+
+interface AbortActionArgs {
+    case: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface AbortResult {
+}
+
 class UtilsController extends BaseEventController {
     name = 'utils'
 
@@ -19,22 +36,31 @@ class UtilsController extends BaseEventController {
         return
     }
 
-    async action (name: string, args: { text: string, pattern: string, case: string }): Promise<any> {
+    async action (name: string, args: MatchActionArgs & AbortActionArgs): Promise<any> {
         logger.debug({ controller: this.name, action: name, args })
         if (name === 'match') {
-            const re = new RegExp(args.pattern, 'mg')
-            const match = re.exec(args.text)
-            if (match) {
-                return match.groups
-            }
-            return {}
+            return await this.matchAction(args)
         } else if (name === 'abort') {
-            if (args.case.toLowerCase() === 'true') {
-                throw new AbortActionError('ABORT')
-            }
+            return await this.abortAction(args)
         } else {
             throw new Error(`unknown action name: ${name}`)
         }
+    }
+
+    async matchAction (args: MatchActionArgs): Promise<MatchResult> {
+        const re = new RegExp(args.pattern, 'mg')
+        const match = re.exec(args.text)
+        if (match) {
+            return match.groups
+        }
+        return {}
+    }
+
+    async abortAction (args: AbortActionArgs): Promise<AbortResult> {
+        if (args.case.toLowerCase() === 'true') {
+            throw new AbortActionError('ABORT')
+        }
+        return {}
     }
 }
 
